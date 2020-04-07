2caad0c078b79cfcd0128d4be129dd518aa761e0
james.pfluger <me@jamespfluger.com>
2020-04-06 22:55:11 -0500

    clarified which project is for user authentication

--- AlexaServices/DeviceApi/Models/UserDevice.cs
+++ AlexaServices/DeviceApi/Models/UserDevice.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AlexaDeviceFinder.Models
+namespace AlexaDeviceFinderAuth.Models
 {
     [DynamoDBTable("UserDevice")]
     public class UserDevice
--- AlexaServices/DeviceApi/Startup.cs
+++ AlexaServices/DeviceApi/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace AlexaDeviceFinder
+namespace AlexaDeviceFinderAuth
 {
     public class Startup
     {
