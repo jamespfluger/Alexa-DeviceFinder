56f27304d17a7654111660eb10fd212413788df3
james.pfluger <me@jamespfluger.com>
2020-04-29 19:25:31 -0500

    upgraded API to .NET Core 3.1

--- AlexaDeviceFinder-API/AlexaDeviceFinder-Auth/LambdaEntryPoint.cs
+++ AlexaDeviceFinder-API/AlexaDeviceFinder-Auth/LambdaEntryPoint.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using Microsoft.Extensions.Hosting;
 
 namespace AlexaDeviceFinderAuth
 {
@@ -12,11 +13,21 @@ namespace AlexaDeviceFinderAuth
     /// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
     /// actual Lambda function entry point. The Lambda handler field should be set to
     /// 
-    /// AlexaDeviceFinder::AlexaDeviceFinder.LambdaEntryPoint::FunctionHandlerAsync
+    /// AWSServerless1::AWSServerless1.LambdaEntryPoint::FunctionHandlerAsync
     /// </summary>
     public class LambdaEntryPoint :
-        // When using an ELB's Application Load Balancer as the event source change 
-        // the base class to Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
+
+        // The base class must be set to match the AWS service invoking the Lambda function. If not Amazon.Lambda.AspNetCoreServer
+        // will fail to convert the incoming request correctly into a valid ASP.NET Core request.
+        //
+        // API Gateway REST API                         -> Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+        // API Gateway HTTP API payload version 1.0     -> Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+        // API Gateway HTTP API payload version 2.0     -> Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
+        // Application Load Balancer                    -> Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
+        // 
+        // Note: When using the AWS::Serverless::Function resource with an event type of "HttpApi" then payload version 2.0
+        // will be the default and you must make Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction the base class.
+
         Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
     {
         /// <summary>
@@ -28,6 +39,17 @@ namespace AlexaDeviceFinderAuth
         {
             builder
                 .UseStartup<Startup>();
+        }
+
+        /// <summary>
+        /// Use this override to customize the services registered with the IHostBuilder. 
+        /// 
+        /// It is recommended not to call ConfigureWebHostDefaults to configure the IWebHostBuilder inside this method.
+        /// Instead customize the IWebHostBuilder in the Init(IWebHostBuilder) overload.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Init(IHostBuilder builder)
+        {
         }
     }
 }
