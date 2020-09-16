759517d0526b0cf1d8d85471665dc5117262cd87
James Pfluger <me@jamespfluger.com>
2020-09-15 17:14:21 -0700

    implemented OTP connection (#43)

--- AlexaDeviceFinder-Android/app/src/main/java/com/jamespfluger/alexadevicefinder/OtpEditText.java
+++ AlexaDeviceFinder-Android/app/src/main/java/com/jamespfluger/alexadevicefinder/OtpEditText.java
@@ -2,11 +2,12 @@ package com.jamespfluger.alexadevicefinder;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
-import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputConnectionWrapper;
@@ -14,45 +15,53 @@ import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.widget.AppCompatEditText;
 
-import java.util.Random;
-
 import static android.content.Context.INPUT_METHOD_SERVICE;
 
-public class OtpEditText  extends AppCompatEditText {
-    private OtpEditText otpField1 = findViewById(R.id.otpField1);
-    private OtpEditText otpField2 = findViewById(R.id.otpField2);
-    private OtpEditText otpField3 = findViewById(R.id.otpField3);
-    private OtpEditText otpField4 = findViewById(R.id.otpField4);
-    private OtpEditText otpField5 = findViewById(R.id.otpField5);
-    private OtpEditText otpField6 = findViewById(R.id.otpField6);
-
-    private final ColorStateList defaultTextColors;
-    private final ColorStateList defaultBackgroundTintList;
+public class OtpEditText extends AppCompatEditText {
+    private ColorStateList defaultTextColors = getTextColors();
 
     public OtpEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        defaultTextColors = getTextColors();
-        defaultBackgroundTintList = getDefaultBackgroundTintList();
     }
 
     public OtpEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        defaultTextColors = getTextColors();
-        defaultBackgroundTintList = getDefaultBackgroundTintList();
     }
 
     public OtpEditText(Context context) {
         super(context);
-        defaultTextColors = getTextColors();
-        defaultBackgroundTintList = getDefaultBackgroundTintList();
     }
 
-    public ColorStateList getDefaultTextColors(){
-        return defaultTextColors;
+    public void setErrorState() {
+        getBackground().mutate().setColorFilter(getResources().getColor(R.color.red), PorterDuff.Mode.SRC_ATOP);
+        setTextColor(getResources().getColor(R.color.red));
     }
 
-    public ColorStateList getDefaultBackgroundTintList(){
-        return defaultBackgroundTintList;
+    public void clearErrorState() {
+        getBackground().mutate().clearColorFilter();
+        setTextColor(defaultTextColors);
+    }
+
+    @Override
+    protected void onFocusChanged(boolean hasFocus, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(hasFocus, direction, previouslyFocusedRect);
+
+        if (hasFocus) {
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    setSelection(getText().length());
+                }
+            });
+
+            ViewGroup viewGroup = (ViewGroup) getParent();
+
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View child = viewGroup.getChildAt(i);
+                if (child instanceof OtpEditText)
+                    ((OtpEditText) child).clearErrorState();
+            }
+        }
     }
 
     @Override
@@ -76,19 +85,17 @@ public class OtpEditText  extends AppCompatEditText {
                 return keyEventResult;
 
             if (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
-                View nextLeftFocus = currentView.focusSearch(currentView.FOCUS_LEFT);
+                View nextLeftFocus = currentView.focusSearch(FOCUS_LEFT);
                 currentView.setText("");
 
                 if (nextLeftFocus != null)
                     nextLeftFocus.requestFocus();
-            }
-            else if (event.getAction() == KeyEvent.ACTION_UP) {
-                View nextRightFocus = currentView.focusSearch(currentView.FOCUS_RIGHT);
+            } else if (event.getAction() == KeyEvent.ACTION_UP) {
+                View nextRightFocus = currentView.focusSearch(FOCUS_RIGHT);
 
                 if (currentView.getText().length() > 0 && nextRightFocus != null) {
                     nextRightFocus.requestFocus();
-                }
-                else if (nextRightFocus == null){
+                } else if (nextRightFocus == null) {
                     InputMethodManager imm = (InputMethodManager) getContext().getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(currentView.getWindowToken(), 0);
                     currentView.clearFocus();
@@ -103,8 +110,7 @@ public class OtpEditText  extends AppCompatEditText {
             if (beforeLength == 1 && afterLength == 0) {
                 return sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL))
                         && sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
-            }
-            else{
+            } else {
                 return super.deleteSurroundingText(beforeLength, afterLength);
             }
         }
