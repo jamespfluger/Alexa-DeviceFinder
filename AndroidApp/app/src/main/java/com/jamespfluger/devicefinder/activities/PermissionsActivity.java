fb90e88babd91cdb9a614397611b006bb4f4bef5
james.pfluger <me@jamespfluger.com>
2021-04-30 19:21:33 -0700

    Implement permission requests

--- AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/activities/PermissionsActivity.java
+++ AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/activities/PermissionsActivity.java
@@ -1,16 +1,74 @@
 package com.jamespfluger.devicefinder.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 import com.jamespfluger.devicefinder.R;
+import com.jamespfluger.devicefinder.controls.PermissionsView;
 
 public class PermissionsActivity extends AppCompatActivity {
+    PermissionsView disableBatteryView;
+    PermissionsView overrideDndView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions);
+
+        disableBatteryView = findViewById(R.id.permissionsDisableBatteryView);
+        overrideDndView = findViewById(R.id.permissionsOverrideDndView);
+    }
+
+    public boolean hasGranteddisableBatteryPermissions() {
+        PowerManager pm = (PowerManager) getApplicationContext().getSystemService(POWER_SERVICE);
+
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || pm.isIgnoringBatteryOptimizations(getPackageName());
+    }
+
+    public boolean hasGrantedDndPermissions() {
+        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || notificationManager.isNotificationPolicyAccessGranted();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (hasGranteddisableBatteryPermissions() && disableBatteryView.getPermissionStatusButton() != null) {
+            disableBatteryView.getPermissionStatusButton().setImageResource(R.drawable.check_circle);
+            disableBatteryView.getPermissionStatusButton().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green));
+        }
+        else if (disableBatteryView.getPermissionStatusButton() != null){
+            disableBatteryView.getPermissionStatusButton().setImageResource(R.drawable.alert_circle);
+            disableBatteryView.getPermissionStatusButton().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.errorred));
+        }
+
+        if (hasGrantedDndPermissions() && overrideDndView.getPermissionStatusButton() != null) {
+            overrideDndView.getPermissionStatusButton().setImageResource(R.drawable.check_circle);
+            overrideDndView.getPermissionStatusButton().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.green));
+        }
+        else if (overrideDndView.getPermissionStatusButton() != null){
+            overrideDndView.getPermissionStatusButton().setImageResource(R.drawable.alert_circle);
+            overrideDndView.getPermissionStatusButton().setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.errorred));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/controls/PermissionsView.java
+++ AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/controls/PermissionsView.java
@@ -1,18 +1,22 @@
 package com.jamespfluger.devicefinder.controls;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.opengl.Visibility;
+import android.provider.Settings;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,6 +25,7 @@ import androidx.databinding.DataBindingUtil;
 import com.jamespfluger.devicefinder.R;
 
 public class PermissionsView extends LinearLayout {
+    private ImageButton permissionStatus;
 
     public PermissionsView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -32,6 +37,10 @@ public class PermissionsView extends LinearLayout {
         init(context, attrs, defStyle);
     }
 
+    public ImageButton getPermissionStatusButton() {
+        return permissionStatus;
+    }
+
     private void init(Context context, AttributeSet attrs, int defStyle) {
         TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.PermissionsView);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -40,9 +49,13 @@ public class PermissionsView extends LinearLayout {
         final Button allowButton = findViewById(R.id.permissionAllowButton);
         final TextView explanationTextView = findViewById(R.id.permissionExplanation);
         final Button expandExplanationButton = findViewById(R.id.permissionExpandButton);
+        permissionStatus = findViewById(R.id.permissionStatusIcon);
 
-        allowButton.setText(attributes.getString(R.styleable.PermissionsView_permissionsTitle));
-        explanationTextView.setText(attributes.getString(R.styleable.PermissionsView_permissionsExplanation));
+        String permissionsTitle = attributes.getString(R.styleable.PermissionsView_permissionsTitle);
+        String permissionsExplanation = attributes.getString(R.styleable.PermissionsView_permissionsExplanation);
+
+        allowButton.setText(permissionsTitle);
+        explanationTextView.setText(permissionsExplanation);
 
         expandExplanationButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -53,6 +66,29 @@ public class PermissionsView extends LinearLayout {
                     explanationTextView.setVisibility(GONE);
             }
         });
+
+        if (permissionsTitle != null && permissionsTitle.equals("Disable Battery Optimization")) {
+            allowButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                        Intent intent = new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+                        v.getContext().startActivity(intent);
+                    }
+                }
+            });
+        }
+        else {
+            allowButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                        Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                        v.getContext().startActivity(intent);
+                    }
+                }
+            });
+        }
 
         attributes.recycle();
 
