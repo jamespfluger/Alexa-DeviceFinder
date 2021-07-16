4ab49b6100412979387de078194832a4d9729027
james.pfluger <me@jamespfluger.com>
2021-07-15 19:00:30 -0700

    Made navigation more intuitive and less buggy

--- AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/activities/PermissionsActivity.java
+++ AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/activities/PermissionsActivity.java
@@ -1,5 +1,6 @@
 package com.jamespfluger.devicefinder.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.navigation.Navigation;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.jamespfluger.devicefinder.R;
+import com.jamespfluger.devicefinder.utilities.AmazonLoginHelper;
 
 public class PermissionsActivity extends AppCompatActivity {
     @Override
@@ -23,9 +25,19 @@ public class PermissionsActivity extends AppCompatActivity {
     public void onBackPressed() {
         new MaterialAlertDialogBuilder(this)
                 .setTitle(R.string.quit)
-                .setMessage(R.string.confirm_quit_question)
-                .setPositiveButton(R.string.yes, (dialog, which) -> PermissionsActivity.super.onBackPressed())
+                .setMessage(R.string.confirm_logout_question)
+                .setPositiveButton(R.string.yes, (dialog, which) -> {
+                    AmazonLoginHelper.signOut(getApplicationContext());
+                    switchToLoginActivity();
+                })
                 .setNegativeButton(R.string.no, null)
                 .show();
+    }
+
+    private void switchToLoginActivity() {
+        Intent newIntent = new Intent(this, LoginActivity.class);
+        startActivity(newIntent);
+        finish();
+        overridePendingTransition(R.transition.slide_in_left, R.transition.slide_out_right);
     }
 }
