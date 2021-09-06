13c142fd5cc46dd6310276e33c28d299f73c9a64
James Pfluger <me@jamespfluger.com>
2021-09-06 15:13:38 -0700

    Add content to 'About' fragment (#109)
    Closes #49

--- AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/activities/fragments/AboutFragment.java
+++ AndroidApp/app/src/main/java/com/jamespfluger/devicefinder/activities/fragments/AboutFragment.java
@@ -1,11 +1,15 @@
 package com.jamespfluger.devicefinder.activities.fragments;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.jamespfluger.devicefinder.R;
@@ -22,5 +26,16 @@ public class AboutFragment extends Fragment {
         }
 
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView issuesView = view.findViewById(R.id.about_issues_content);
+        TextView contributingView = view.findViewById(R.id.about_contributing_content);
+
+        issuesView.setMovementMethod(LinkMovementMethod.getInstance());
+        contributingView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
